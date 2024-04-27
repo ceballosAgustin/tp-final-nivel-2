b1fc1d76e9e504c77f0a5fdcb4545f55bdd333c1
Agustín Ceballos <aceballos1923@gmail.com>
2024-04-27 19:13:57 -0300

    Conectar las Categorias con la DB

---
+++ presentacion/CategoriasNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dominio;
+using negocio;
+
+namespace presentacion
+{
+    public class CategoriasNegocio
+    {
+        public List<Categorias> listar()
+        {
+            List<Categorias> lista = new List<Categorias>();
+
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id, Descripcion from CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categorias aux = new Categorias();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
